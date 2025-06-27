b78741d2ff121db68ded23fbfa62ce622fb6cbf0
Sarthak Das <sdas4@icloud.com>
2025-06-27 14:42:01 +0100

    Installation Fix
    * MacOS Install FIx
    
    * GItlab update

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
-
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
 
 class CustomInstallCommand(install):
     """Custom handler for the install command to run make before installation."""
@@ -10,10 +10,19 @@ class CustomInstallCommand(install):
     def run(self):
         # Run the make command
         subprocess.check_call(["make"])
-        
+
         # Continue with the standard installation
         super().run()
 
+class CustomBuildPyCommand(build_py):
+    def run(self):
+        subprocess.check_call(["make"])
+        super().run()
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        subprocess.check_call(["make"])
+        super().run()
 
 setup(
     name="arktypes",
@@ -24,5 +33,7 @@ setup(
     ],
     cmdclass={
         'install': CustomInstallCommand,
+        'build_py': CustomBuildPyCommand,
+        'develop': CustomDevelopCommand,
     },
 )
